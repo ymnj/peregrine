e3ead06efdbfeaee6cfdf8cb3eca037f19f59712
Tom <tomhung87@gmail.com>
2016-04-15 12:52:01 -0700

    Test rake compile before heroku push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
 
 
   config.serve_static_files = true
-  config.assets.compile = true
 
 
   # Code is not reloaded between requests.
@@ -36,7 +35,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
