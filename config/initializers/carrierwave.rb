c05d810b687d45b90abc6e65a8505a656899c625
Tom <tomhung87@gmail.com>
2016-04-18 00:37:02 -0700

    Fix endpoint uri in carrierwave config.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     Rails.application.secrets.AWS_ACCESS_KEY_ID || ENV["AWS_ACCESS_KEY_ID"],                        # required
     aws_secret_access_key: Rails.application.secrets.AWS_SECRET_ACCESS_KEY || ENV["AWS_SECRET_ACCESS_KEY"],                        # required
-    region:                'US Standard'                 # optional, defaults to 'us-east-1'
+    region:                'us-east-1'                 # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = "peregrine-app"                          # required
   config.fog_public     = false
